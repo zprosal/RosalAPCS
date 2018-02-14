92924da74919da087763a2c45039715bff822042
Zechariah Rosal <Rosal.Zech2018@ssusd.org>
2018-02-13 16:27:57 -0800

    Boolean exercise

--- Functions/ValidationUtils.java
+++ Functions/ValidationUtils.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 class ValidationUtils {
     public static void main(String args[]) {
+        // String
         System.out.print("Enter a string: ");
         Scanner scan1 = new Scanner(System.in);
         if(isValidString(scan1)) {
@@ -10,7 +11,7 @@ class ValidationUtils {
         else {
             System.out.println("String Failure!");
         }
-
+        // Integer
         System.out.print("Enter a number (decimals not allowed): ");
         Scanner scan2 = new Scanner(System.in);
         if(isValidInt(scan2)) {
@@ -19,7 +20,7 @@ class ValidationUtils {
         else {
             System.out.println("Integer Failure!");
         }
-
+        // Double
         System.out.print("Enter a number (decimals allowed): ");
         Scanner scan3 = new Scanner(System.in);
         if(isValidDouble(scan3)) {
@@ -27,6 +28,15 @@ class ValidationUtils {
         }
         else {
             System.out.println("Double Failure!");
+        }
+        // Boolean
+        System.out.print("Enter a Boolean(T,F,true,false): ");
+        Scanner scan4 = new Scanner(System.in);
+        if(isValidBoolean(scan4)) {
+            System.out.println("Boolean Success!");
+        }
+        else {
+            System.out.println("Boolean Failure!");
         }
     }
 
@@ -40,6 +50,7 @@ class ValidationUtils {
         }
         return true; //No error was raised. The scanner read it successfully as a String.
     }
+
     public static Boolean isValidInt(Scanner scan2) {
         try {
             int x = scan2.nextInt();
@@ -59,5 +70,15 @@ class ValidationUtils {
         }
         return true;
     }
-    // s.tolowerCase() gets rid of captial letters
+
+    public static Boolean isValidBoolean(Scanner scan4) {
+        try {
+            String b = scan4.nextLine(),tolowerCase;
+        }
+        catch(Exception e) {
+            return false; 
+        }
+        return true; 
+    }
+
 }
