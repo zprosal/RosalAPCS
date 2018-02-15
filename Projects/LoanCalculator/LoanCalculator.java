a68e932bccc5cd00b72a05a92f2ae216f6671ce6
Zechariah Rosal <Rosal.Zech2018@ssusd.org>
2018-02-15 07:36:49 -0800

    Actual Progress on Loan Calculator

--- Projects/LoanCalculator/LoanCalculator.java
+++ Projects/LoanCalculator/LoanCalculator.java
@@ -8,19 +8,55 @@ public class LoanCalculator{
 
     public static void main(String[] args) {
         //Read in the loan calculation mode
-        Scanner scan;
+        Scanner scan1;
         while(true) { //Loop the mode selection menu until a valid input is provided
-            scan = new Scanner(System.in);
+            scan1 = new Scanner(System.in);
             printModeStatement(); //Call a function to print our mode selection statement
-            if(scan.hasNextInt()) { //Check if input provided is a valid integer
-                mode = scan.nextInt();
+            if(scan1.hasNextInt()) { //Check if input provided is a valid integer
+                mode = scan1.nextInt();
                 if(0 < mode && mode < 4) { //Check if input provided is 1, 2, or 3
                     break; //Valid input has been provided. Break out of the while loop
                 }
             }
             //If it reaches here, the input was invalid. Loop again.
         }
+        
         System.out.println("You selected mode " + mode);
+        Scanner scan2;
+        while(true) { 
+            scan2 = new Scanner(System.in);
+            System.out.println("Enter Loan Amount: ");             
+            if(scan2.hasNextDouble()) { 
+                loanAmt = scan2.nextDouble();
+                if(loanAmt > 0) { 
+                    break; 
+                }
+            }
+        }
+        Scanner scan3;
+        while(true) { 
+            scan3 = new Scanner(System.in);
+            System.out.println("Enter Loan Term (in years): "); 
+            if(scan3.hasNextInt()) { 
+                loanTerm = scan3.nextInt();
+                if(loanTerm > 0) { 
+                    break; 
+                }
+            }
+
+        }
+        Scanner scan4;   
+        while(true) { 
+            scan4 = new Scanner(System.in);
+            System.out.println("Enter Interest Rate (%): "); 
+            if(scan4.hasNextDouble()) { 
+                interestRate = scan4.nextDouble();
+                if(interestRate > 0) { 
+                    break; 
+                }
+            }
+
+        }
 
         if(mode == 1) {
             flatInterest();
@@ -33,12 +69,16 @@ public class LoanCalculator{
     public static void flatInterest() {
         System.out.println("Still a stub function. Not yet implemented"); //Delete this once you implement the function
         //Calculate flat interest and print it out here
-        
+        System.out.println(
+            "Original Loan Amount:\n" + loanAmt + "\n"
+            + "Loan Term\n" + loanTerm + "\n"
+            + "Interest Rate\n" + interestRate
+        );
     }
 
     public static void printModeStatement() {
         System.out.print(
-            "Select an interest calculation mode:\n"
+            "Select an interest calculation mode:\n" // "\n" is new line
             + "(1) Flat Interest\n"
             + "(2) Compounding Interest Without Monthly Payments\n"
             + "(3) Compounding Interest With Monthly Payments\n"
