4f63b72adf85b44a4c25707829ae3f129bf09126
Zechariah Rosal <Rosal.Zech2018@ssusd.org>
2018-02-21 07:55:36 -0800

    Formatting Exercise

---
+++ Assignments/Exercises/Formatting.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+// clean up the formatting
+class Formatting {
+    public static void main(String args[]) {
+        System.out.print("Enter a string: ");
+        Scanner scan = new Scanner(System.in);
+        if(isValidString(scan)) {
+            System.out.println("String Success!");
+        }
+        else {
+            System.out.println("String Failure!");
+        }
+    }
+        
+    public static Boolean isValidString(Scanner scan) {
+        try {
+            String s = scan.nextLine();
+        }
+        catch(Exception e) {
+            return false;
+        }
+        return true;
+    }
+}
