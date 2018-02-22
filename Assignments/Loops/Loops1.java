3dfb4076eeb9448908ac54a5637f4d9afbdfc9c4
Zechariah Rosal <Rosal.Zech2018@ssusd.org>
2018-02-22 07:37:47 -0800

    While Loops exercise

---
+++ Assignments/Loops/Loops1.java
@@ -0,0 +1,67 @@
+class Loops1 {
+    public static void main(String[] args) {
+// 1.        
+    // print endless 0s
+        // int n = 0;
+        // while(n < 100) {
+        //     System.out.println(n);
+        //     n *= 5;
+        // } 
+    // prints mutiples of 5
+        // int n = 0;
+        // while(n < 100) {
+        //     System.out.println(n);
+        //     n += 5;
+        // }              
+        // print 19 (incorrect; prints nothing)
+        // int n = 19;
+        // while(n % 2 == 0) {
+        //     System.out.println(n);
+        //     n--;        
+        // }        
+    // print the Fibonacci sequence under 100 (incorrect; prints powers of 2 starting with 2^0) 
+        // int n = 1;
+        // while(n < 100) {
+        //     System.out.println(n);
+        //     n += n;        
+        // }        
+    // prints 1-20
+        // Boolean shouldLoop = true;
+        // int n = 0;
+        // while(shouldLoop) {
+        //     if(n == 20) {
+        //         shouldLoop = false;
+        //     }
+        //     System.out.println(n);
+        //     n++;
+        //     } 
+    // prints "Hello World" 10 times
+        // int n = 0;
+        // while(true) {
+        //     System.out.println("Hello World");
+        //     n++;
+        //     if(n == 10){
+        //         break;
+        //     }
+        // }          
+// 2.             
+    // prints endless 1s, n does not change
+    //     int n = 1;
+    //     while(n < 10) {
+    //         System.out.println(n);
+    //         n++; //fix, should print 1-9
+    //     }
+    // prints "100 loop passes occured." every 100, no break is present
+        // Boolean shouldLoop = true;
+        // int n = 1;
+        // while(shouldLoop) {
+        //     if(n == 100) {
+        //         System.out.println("100 loop passes occured.");
+        //         break; //fix, should not repeat
+        //     }
+        //     n++;
+        // }    
+// 3.
+
+    }
+}
