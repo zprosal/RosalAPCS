95d2bfb8893b8fb46a7f4ab5a5d31b76ff3b0cca
Zechariah Rosal <Rosal.Zech2018@ssusd.org>
2018-03-09 07:38:04 -0800

    Arrays

---
+++ Assignments/Arrays/Arrays.java
@@ -0,0 +1,26 @@
+class Arrays {
+    public static void main(String[] args) {
+    // 1. 
+        // int[] numArray = {20, 3, 6, 10, 13, 25, 100, 6, 3, 20, 5102};
+
+        // System.out.println( numArray[3] ); //6 (actually 10)
+
+        // // System.out.println( numArray[11] );//5102 (actually broken)
+
+        // System.out.println( numArray[1] ); //20 (actually 3)
+
+        // System.out.println( numArray[7] ); //100 (actually 6)
+
+        // System.out.println( numArray[8] ); //6 (actually 3)
+    // 2.
+        // System.out.println( numArray[4]);
+        // System.out.println( numArray[1]);
+        // System.out.println( numArray[10]);
+        // System.out.println( numArray[5]);
+        // System.out.println( numArray[0]);
+    // 3.
+        int[] fibonacci = {1, 1, 2, 3, 0, 8, 13, 0, 34, 0};
+        fibonacci[4] = fibonacci[2] + fibonacci[3];
+        System.out.println(fibonacci[4]);
+    }
+}
--- Assignments/ScopeTest/ScopeTest.java
+++ Assignments/ScopeTest/ScopeTest.java
@@ -1,30 +1,57 @@
 class ScopeTest {
-        public static String message = "Hello";
-        public static Boolean shouldLoop = false;
+    // 1.
+        // public static String message = "Hello";
 
-        public static void main(String args[]) {
-            int i = 1;
+        // public static void main(String args[]) {
+        //     int i = 0;
+        //     int n = 0;
+        //     while(i <= 10) {
+        //         n = i * 2;
+        //         System.out.println(n);
+        //         i++;
+        //     }
 
-            while(i <= 10) {
-                int n = i * 2;
-                System.out.println(n);
-                i++;
-            }
-            int n = i * 2;
-            if(n > 10) {
-                String message = "Hello World";
-                System.out.println(message);
-            }
+        //     if(n > 10) {
+        //         String message = "Hello World";
+        //         System.out.println(message);
+        //     }
 
-            shouldLoop = true;
-            i = 0;
-            while(shouldLoop) {
-                if(i < 10) {
-                    System.out.println(message);                    
-                } else {
-                    shouldLoop = false;
-                }
-                i++;
+        //     Boolean shouldLoop = true;
+        //     i = 0;
+        //     while(shouldLoop) {
+        //         if(i < 10) {
+        //             System.out.println(message);                    
+        //         } else {
+        //             shouldLoop = false;
+        //         }
+        //         i++;
+        //     }
+        
+    // 2.
+            // String message = "Hello World";
+            // int i;
+            // for(i = 0; i < message.length(); i++) {
+            //     if(message.charAt(i) == 'W') {
+            //         break;
+            //     }
+            // }
+            // System.out.println("W found at index " + i);
+    // 3.            
+        public static final int n = 32;
+        public static int base = 2;
+
+        public static void main(String[] args) {
+            for(int i = 0; i <= n; i++) {
+                System.out.println(power(i));
             }
         }
+
+        public static int power(int power) {
+            int result = 1;
+            for(int  i = 0; i < power; i++) {
+                result *= base;
+            }
+            return result;
+        }        
+    //     }
     }
