e1ae4ff8210abbf738e44bec1c3f1954a2bde1be
Zechariah Rosal <Rosal.Zech2018@ssusd.org>
2018-04-10 07:38:33 -0700

    2Darray

--- Assignments/2DArrays/TwoDArrays.java
+++ Assignments/2DArrays/TwoDArrays.java
@@ -1,4 +1,4 @@
-class 2DArrays {
+class TwoDArrays {
     public static void main(String[] args) {
     // 1.
         //Example - Contents of a 3x3 array of zeros
@@ -27,17 +27,32 @@ class 2DArrays {
         //[0,16,144]
         //[0,144,2304]
 
-        int[][] arr3 = new int[5][5];
+        int[][] arr3 = new int[4][4];
 
         int num = 1;
         for(int i = 0; i < arr3.length; i++) {
             for(int j = 0; j < arr3[i].length; j++) {
                 arr3[i][j] = num; 
-                num *= 2
+                num *= 2;
             }
         }
-        //[1,2,4,8,16]
-        //[32,64,128,256,512]
-        //[1024,2048,4096,8192,16384]    
+        //[1,2,4,8]
+        //[16,32,64,128]
+        //[256,512,1024,2048]
+        //[4096,8192,16384,32768]  
+    // 2.
+        int[][] arr4 = new int[3][3];
+        int num2 = 1; 
+        for(int i = 0; i < arr4.length; i++) {
+            for(int j = 0; j < arr4[i].length; j++) {
+                arr4[i][j] = num2;
+                num2 *= 3;
+            }
+        }
+
+        int[][] arr5 = new int[5][5]
+        for(int i = 0; i < arr5.length; i++) {
+            for(int j = 0; j < arr5[i].length)
+        } 
     }
 }
